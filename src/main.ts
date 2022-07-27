37bd202b34f5911b4b5ce9bd0e63f0311547ca31
Helmer <11061182+helmerdavila@users.noreply.github.com>
2022-07-27 18:09:48 -0400

    First version deployed

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,29 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { AppService } from './app.service';
+import { HttpStatus } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { Handler } from "aws-lambda";
+import { AppModule } from "./app.module";
+import { AppService } from "./app.service";
+// import AWS from "aws-sdk";
 
-async function bootstrap() {
+export const handler: Handler = async () => {
   const app = await NestFactory.createApplicationContext(AppModule);
   const appService = app.get(AppService);
-  console.log(appService.getHello());
-}
-bootstrap();
+  // const dynamoDB = new AWS.DynamoDB.DocumentClient();
+
+  return {
+    body: JSON.stringify({ message: appService.getHello() }),
+    statusCode: HttpStatus.OK,
+  };
+};
+
+export const handlerPost: Handler = async () => {
+  const app = await NestFactory.createApplicationContext(AppModule);
+  const appService = app.get(AppService);
+
+  return {
+    body: JSON.stringify({
+      message: `Hello from post ${appService.getHello()}`,
+    }),
+    statusCode: HttpStatus.OK,
+  };
+};
